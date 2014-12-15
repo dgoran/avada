00cf32f721cb8918c4ad6f9d428f78c55af2047a
MilanPavicc <milanpavice@hotmail.com>
2014-12-15 22:02:06 +0100

    Child theme opet

---
+++ themes/Avada-Theme-child-01/gulpfile.js
@@ -0,0 +1,26 @@
+var gulp=require('gulp');
+var plumber=require('gulp-plumber');
+var autoprefixer=require('gulp-autoprefixer');
+var sass=require('gulp-ruby-sass');
+var maps=require('gulp-sourcemaps');
+var livereload=require('gulp-livereload');
+
+// Css tsk
+gulp.task('css', function(){
+  return gulp.src('css/scss/main.scss')
+          .pipe(plumber())
+          .pipe(sass({sourcemap: true, sourcemapPath: 'scss/'}))
+          .pipe(gulp.dest('css/'))
+          .pipe(livereload());
+          
+
+});
+
+// Watch for css
+gulp.task('watch',function(){
+    var server=livereload();
+    gulp.watch('css/scss/*.scss',['css']);
+});
+
+/*Run css task and watch it*/
+gulp.task('default',['css','watch']);
