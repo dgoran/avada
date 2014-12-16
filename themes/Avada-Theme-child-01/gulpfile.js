b531a4c2ef5478cada8294d2610fdcdf3d9786b6
MilanPavicc <milanpavice@hotmail.com>
2014-12-16 16:42:04 +0100

    asd233

--- themes/Avada-Theme-child-01/gulpfile.js
+++ themes/Avada-Theme-child-01/gulpfile.js
@@ -1,15 +1,21 @@
 var gulp=require('gulp');
 var plumber=require('gulp-plumber');
 var autoprefixer=require('gulp-autoprefixer');
-var sass=require('gulp-ruby-sass');
-var maps=require('gulp-sourcemaps');
+/*var sass=require('gulp-ruby-sass');*/
+/*var maps=require('gulp-sourcemaps');*/
 var livereload=require('gulp-livereload');
+/*var compass = require('gulp-compass');*/
 
 // Css tsk
 gulp.task('css', function(){
   return gulp.src('css/scss/main.scss')
           .pipe(plumber())
           /*.pipe(sass({sourcemap: true, sourcemapPath: 'scss/'}))*/
+         /* .pipe(compass({
+      config_file: './config.rb',
+      css: 'css',
+      sass: 'css/scss'
+    }))*/
           .pipe(gulp.dest('css/'))
           .pipe(livereload());
           
